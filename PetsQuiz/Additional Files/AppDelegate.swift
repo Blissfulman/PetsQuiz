f08d950887fda01a0f3dfa7472470ed5f93ded04
Evgeny <greatly01@ya.ru>
2020-11-06 22:44:38 +0500

    Empty project

---
+++ PetsQuiz/Additional Files/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  PetsQuiz
+//
+//  Created by User on 06.11.2020.
+//  Copyright © 2020 Swiftbook. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
